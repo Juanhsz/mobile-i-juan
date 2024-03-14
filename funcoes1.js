27fdbdd71a33fa34241c691bce2f0982ed069bdb
Juanhsz <juan.hernandes@etec.sp.gov.br>
2024-03-14 08:26:25 -0300

    Aula 03 sobre métodos e funções.

---
+++ funcoes1.js
@@ -0,0 +1,16 @@
+function Soma (){
+    let n1 = 2;
+    let n2 = 6;
+
+    console.log(n1+n2);
+
+}
+
+Soma()
+
+function SomaComParametros (n1, n2) {
+    console.log(n1+n2)
+}
+
+SomaComParametros(4, 10)
+SomaComParametros(390595, 230597)
